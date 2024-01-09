48487141c22e8f164e004f53014da38b179e2ddf
Vinicius Alves <vinialves.dev@gmail.com>
2024-01-09 20:51:25 -0300

    feat: add new services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CreateProductController } from './controllers/createProduct.controller';
 import { CreateProductService } from './services/createProduct.service';
+import { ListProductController } from './controllers/listProduct.controller';
+import { ListProductsService } from './services/listProducts.service';
+import { ProductRepository } from './repositories/product.repository';
+import { ProductsDatabase } from './database/products.database';
 
 @Module({
   imports: [],
-  controllers: [CreateProductController],
-  providers: [CreateProductService],
+  controllers: [CreateProductController, ListProductController],
+  providers: [
+    CreateProductService,
+    ListProductsService,
+    ProductRepository,
+    ProductsDatabase,
+  ],
 })
 export class AppModule {}
