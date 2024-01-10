e50c631ff133c54c423f4c3417ebeffa7c8c6467
Vinicius Alves <vinialves.dev@gmail.com>
2024-01-10 18:22:34 -0300

    feat: func create products

--- src/controllers/createProduct.controller.ts
+++ src/controllers/createProduct.controller.ts
@@ -1,10 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Req, Res } from '@nestjs/common';
 import { CreateProductService } from '../services/createProduct.service';
+import { Request, Response } from 'express';
+import { ProductDTO } from 'src/dto/product.dto';
 
 @Controller('product')
 export class CreateProductController {
+  private readonly logger = new Logger(CreateProductController.name);
+
   constructor(private readonly createProductService: CreateProductService) {}
 
   @Post()
-  handle(): void {}
+  async handle(
+    @Body() productDto: ProductDTO,
+    @Res() res: Response,
+  ): Promise<void> {
+    try {
+      await this.createProductService.execute(productDto);
+
+      res.sendStatus(201);
+    } catch (error) {
+      this.logger.error(`Unable to register product. \nReason: ${error}`);
+      res.sendStatus(500);
+    }
+  }
 }
--- src/services/createProduct.service.ts
+++ src/services/createProduct.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ProductDTO } from 'src/dto/product.dto';
+import { Product } from 'src/entities/product.entity';
+import { ProductRepository } from 'src/repositories/product.repository';
 
 @Injectable()
 export class CreateProductService {
-  execute(): void {}
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async execute(productData: ProductDTO): Promise<void> {
+    const productEntity = new Product();
+
+    Object.assign(productEntity, productData);
+
+    this.productRepository.save(productEntity);
+  }
 }
